fa4ab56fe3802d0592249ad4d8dfa56a00ca209b
c-wind <mrt@codecn.org>
2017-03-06 23:08:19 +0800

    test ok

--- config/config.go
+++ config/config.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"reflect"
-	//"strconv"
+	"strconv"
 	"strings"
-	"sync"
 	"unicode"
 )
 
 var (
-	ErrExpectPtr = fmt.Errorf("expect ptr data")
+	Errunsupported = fmt.Errorf("expect ptr data")
+	ErrInvalidType = fmt.Errorf("result must be ptr")
 )
 
 // Config 读ini配置文件.
 type Config struct {
 	kv map[string]string
-	mu sync.RWMutex
 }
 
 //TrimSpace 删除头尾的空字符，空格，换行之类的东西, 具体在unicode.IsSpac.
@@ -51,23 +50,6 @@ func TrimSplit(raw, sep string) []string {
 	return ss[:i]
 }
 
-//trim 清理注释, 空格.
-func trim(line string) string {
-	b := '\n'
-	for i, v := range line {
-		switch v {
-		case '/':
-			if b == '/' {
-				return line[:i]
-			}
-		case '#':
-			return line[:i]
-		}
-		b = v
-	}
-	return TrimSpace(line)
-}
-
 //NewConfig 加载配置文件.
 func NewConfig(f string) (c *Config, err error) {
 	dat, err := ioutil.ReadFile(f)
@@ -79,8 +61,12 @@ func NewConfig(f string) (c *Config, err error) {
 	s := ""
 
 	for _, line := range TrimSplit(string(dat), "\n") {
-		line = trim(line)
-		if line[0] == '[' && line[len(line)-1] == ']' {
+		if e := strings.Index(line, "#"); e > 0 {
+			line = line[:e]
+		}
+		line = TrimSpace(line)
+
+		if len(line) > 2 && line[0] == '[' && line[len(line)-1] == ']' {
 			s = line[1 : len(line)-1]
 			continue
 		}
@@ -96,17 +82,14 @@ func NewConfig(f string) (c *Config, err error) {
 }
 
 func makeKey(s, k string) string {
-	return fmt.Sprintf("%s_%s", s, k)
+	return strings.ToLower(fmt.Sprintf("%s_%s", s, k))
 }
 
 //GetData 获取指定段的指定key的值, 支持int,string.
 func (c *Config) GetData(s, k string, result interface{}) error {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-
 	rt := reflect.TypeOf(result)
 	if rt.Kind() != reflect.Ptr {
-		return ErrExpectPtr
+		return ErrInvalidType
 	}
 	rt = rt.Elem()
 	rv := reflect.ValueOf(result).Elem()
@@ -118,17 +101,82 @@ func (c *Config) GetData(s, k string, result interface{}) error {
 		return nil
 	}
 
-	//var data interface{}
-    var err error
 	switch rt.Kind() {
-	case reflect.Int:
-		//data, err = strconv.Atoi(v)
-    case reflect.String:
-	rv.SetString(v)
-//        data = &v
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		data, err := strconv.ParseUint(v, 10, 64)
+		if err != nil {
+			return err
+		}
+		rv.SetUint(data)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		data, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return err
+		}
+		rv.SetInt(data)
+
+	case reflect.String:
+		rv.SetString(v)
+
+	case reflect.Bool:
+		data, err := strconv.ParseBool(v)
+		if err != nil {
+			return err
+		}
+		rv.SetBool(data)
+	case reflect.Float32, reflect.Float64:
+		data, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return err
+		}
+		rv.SetFloat(data)
+	default:
+		return Errunsupported
 	}
 
-//	rv.Set(reflect.ValueOf(data))
+	return nil
+}
 
-	return err
+func LoadConfig(f string, result interface{}) error {
+	c, err := NewConfig(f)
+	if err != nil {
+		return err
+	}
+
+	rt := reflect.TypeOf(result)
+	rv := reflect.ValueOf(result)
+
+	if rt.Kind() != reflect.Ptr {
+		return ErrInvalidType
+	}
+
+	//去指针
+	if rt.Kind() == reflect.Ptr && rt.Elem().Kind() == reflect.Struct {
+		rt = rt.Elem()
+		rv = rv.Elem()
+	}
+
+	for i := 0; i < rt.NumField(); i++ {
+		f := rt.Field(i)
+		if f.PkgPath != "" && !f.Anonymous { // unexported
+			continue
+		}
+		fv := rv.Field(i)
+
+		if f.Type.Kind() == reflect.Struct {
+			for j := 0; j < f.Type.NumField(); j++ {
+				sf := f.Type.Field(j)
+				if f.PkgPath != "" && !f.Anonymous { // unexported
+					continue
+				}
+				sfv := fv.Field(j)
+
+				if err = c.GetData(f.Name, sf.Name, sfv.Addr().Interface()); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- config/config_test.go
+++ config/config_test.go
@@ -1,20 +1,64 @@
 package config
 
 import (
+	"os"
 	"testing"
 )
 
-func TestConfig(t *testing.T) {
-	c, err := NewConfig("./test.ini")
-	if err != nil {
+var (
+	c *Config
+)
+
+func TestMain(main *testing.M) {
+	var err error
+	if c, err = NewConfig("./test.ini"); err != nil {
+		panic(err.Error())
+	}
+	os.Exit(main.Run())
+}
+
+func TestConfigString(t *testing.T) {
+	var domain string
+	if err := c.GetData("db", "domain", &domain); err != nil {
 		t.Fatalf(err.Error())
 	}
+	t.Logf("domain:%v", domain)
+}
 
-    t.Logf("config:%+v", *c)
-    var domain string
-    if err = c.GetData("db", "domain", &domain); err != nil {
+func TestConfigInt(t *testing.T) {
+	var port int
+	if err := c.GetData("db", "port", &port); err != nil {
 		t.Fatalf(err.Error())
-    }
-    t.Logf("domain:%v", domain)
+	}
+	t.Logf("port:%d", port)
+}
 
+func TestConfigBool(t *testing.T) {
+	var enable bool
+	if err := c.GetData("db", "enable", &enable); err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("enable:%v", enable)
+}
+
+type testConf struct {
+	DB struct {
+		Domain string
+		Port   int
+		Enable bool
+	}
+
+	API struct {
+		URL        string
+		Enable     bool
+		HeaderSize int
+	}
+}
+
+func TestConfigStruct(t *testing.T) {
+	var conf testConf
+	if err := LoadConfig("./test.ini", &conf); err != nil {
+		t.Fatalf(err.Error())
+	}
+	t.Logf("conf:%+v", conf)
 }
