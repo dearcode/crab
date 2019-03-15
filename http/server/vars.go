c9a5253a8fc548ae2a8f220c95de376a8be5604f
Cwind <root@codecn.org>
2019-03-15 16:52:56 +0800

    添加通用解析参数功能

--- http/server/vars.go
+++ http/server/vars.go
@@ -89,6 +89,12 @@ func UnmarshalJSON(req *http.Request, result interface{}) error {
 	if err != nil {
 		return errors.Trace(err)
 	}
+
+	//空body不解析，不报错
+	if len(data) == 0 {
+		return nil
+	}
+
 	return json.Unmarshal(data, result)
 }
 
@@ -130,7 +136,77 @@ func ParseJSONVars(req *http.Request, result interface{}) error {
 	return UnmarshalValidate(req, JSON, result)
 }
 
-//ParseURLVars 解析并验证头中参数.
+//ParseVars 通用解析，先解析url,再解析body,最后验证结果
+func ParseVars(req *http.Request, result interface{}) error {
+	if result == nil {
+		return meta.ErrArgIsNil
+	}
+
+	if err := ParseURLVars(req, result); err != nil {
+		return errors.Trace(err)
+	}
+
+	if err := UnmarshalJSON(req, result); err != nil {
+		return errors.Trace(err)
+	}
+
+	valid := validation.Validation{}
+	_, err := valid.Valid(result)
+	return errors.Trace(err)
+}
+
+//ParseURLVars 解析url中参数.
 func ParseURLVars(req *http.Request, result interface{}) error {
-	return UnmarshalValidate(req, URI, result)
+	rt := reflect.TypeOf(result)
+	rv := reflect.ValueOf(result)
+
+	//去指针
+	if rt.Kind() == reflect.Ptr && rt.Elem().Kind() == reflect.Struct {
+		rt = rt.Elem()
+		rv = rv.Elem()
+	}
+
+	for i := 0; i < rt.NumField(); i++ {
+		f := rt.Field(i)
+		if f.PkgPath != "" && !f.Anonymous { // unexported
+			continue
+		}
+		key := f.Tag.Get("json")
+		if key == "" {
+			key = f.Name
+		}
+
+		val := req.URL.Query().Get(key)
+		if val == "" {
+			val = req.Header.Get(key)
+		}
+
+		switch f.Type.Kind() {
+		case reflect.Bool:
+			vb, err := strconv.ParseBool(val)
+			if err != nil {
+				return fmt.Errorf("key:%v value:%v format error", key, val)
+			}
+			rv.Field(i).SetBool(vb)
+
+		case reflect.Int, reflect.Int64:
+			vi, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				return fmt.Errorf("key:%v value:%v format error", key, val)
+			}
+			rv.Field(i).SetInt(vi)
+
+		case reflect.Uint, reflect.Uint64:
+			vi, err := strconv.ParseUint(val, 10, 64)
+			if err != nil {
+				return fmt.Errorf("key:%v value:%v format error", key, val)
+			}
+			rv.Field(i).SetUint(vi)
+
+		case reflect.String:
+			rv.Field(i).SetString(val)
+		}
+	}
+
+	return nil
 }
